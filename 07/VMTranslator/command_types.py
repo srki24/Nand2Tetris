723268e899d35a0a6085a7e6e614aff65bbfff6c
srki24 <25962029+srki24@users.noreply.github.com>
2024-05-12 16:36:53 +0200

    rest of the command types

--- 07/VMTranslator/command_types.py
+++ 07/VMTranslator/command_types.py
@@ -4,11 +4,11 @@ from enum import Enum
 class CommandType(Enum):
 
     C_ARITHMETICS = r"^add$|^sub$|^neg$|^eq$|^gt$|^lt$|^and$|^or$|^not$"
-    C_PUSH = r"^(?:push)(?:\s\w+){2}$" # Push arg1 arg2
-    C_POP = r"^(?:pop)(?:\s\w+){2}$"    # Pop arg1 arg2   # r"^pop$|^pop(?:\s\w+)+"
-    C_LABEL = 4
-    C_GOTO = 5
-    C_IF = 6
-    C_FUNCTION = 7
-    C_RETURN = 8
-    C_CALL = 9
+    C_PUSH = r"^(?:push)(?:\s\w+){2}$"         # Push arg1 arg2
+    C_POP = r"^(?:pop)(?:\s\w+){2}$"           # Pop arg1 arg2 
+    C_LABEL = r"^(?:label)(?:\s\w+){1}$"       # label label_name
+    C_GOTO = r"^(?:goto)(?:\s\w+){1}$"         # goto label
+    C_IF = r"^(?:if-goto)(?:\s\w+){1}$"        # if-goto label
+    C_FUNCTION = r"^(?:function)(?:\s\S+){2}$" # function fn.name nr of args
+    C_RETURN = r"^(?:return)$"                 # return
+    C_CALL = r"^(?:call)(?:\s\S+){2}$"         # call fn.name nr of args
